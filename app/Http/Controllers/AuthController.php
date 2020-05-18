2dcfadaa934b05eebe47b03d8e8b82e5bfea2ccf
mohamadmeksasi <mouhamadmeksasi@gmail.com>
2020-05-18 16:27:06 +0300

    adding extra time for token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type'   => 'bearer',
-            'expires_in'   => auth()->factory()->getTTL() * 160
+            'expires_in'   => auth()->factory()->getTTL() * 20000
         ]);
     }
 }
